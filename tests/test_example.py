59a7e09fb7e2bbd4b1317145d7d4c2f8c5a62927
Romapsp <surkov.ron@gmail.com>
2026-05-06 18:57:29 +0300

    add duplicate test

--- tests/test_example.py
+++ tests/test_example.py
@@ -16,4 +16,11 @@ def test_get_started_link(page: Page):
     # Expects page to have a heading with the name of Installation.
     expect(page.get_by_role("heading", name="Installation")).to_be_visible()
 
-    
+def test_get_started_link_2(page: Page):
+    page.goto("https://playwright.dev/")
+
+    # Click the get started link.
+    page.get_by_role("link", name="Get started").click()
+
+    # Expects page to have a heading with the name of Installation.
+    expect(page.get_by_role("heading", name="Installation")).to_be_visible()    
